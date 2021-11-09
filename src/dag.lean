a967d50d697872c43de62a45115f8bdac5af32ce
Alex J. Best <alex.j.best@gmail.com>
2021-11-09 16:50:36 +0100

    init

---
+++ src/dag.lean
@@ -0,0 +1,267 @@
+import meta.rb_map
+variables (T : Type) [has_lt T] [decidable_rel ((<) : T → T → Prop)]
+meta def dag : Type := native.rb_lmap T T
+-- TODO remove has_to_strings that were only needed for tracing
+
+namespace dag
+-- private meta def reflect_dag {X Y : Type} [has_reflect X] [has_reflect Y] : has_reflect (X × Y)
+-- | ns := `(id %%(r.mkp `(prod.mk [ ns.1, ns.2]) : X × Y)
+-- private meta def reflect_dag [has_reflect T] : has_reflect (dag T) | ns :=
+--  expr.mk_app `(native.rb_map.of_list) (list.reflect ((native.rb_map.fold ns [] (λ a b o, (a, b) :: o)))
+-- `(id %%(expr.mk_app `(Prop) $ ns.map (flip expr.const [])) : dag T)
+
+meta def mk : dag T := native.rb_lmap.mk T T
+variable {T}
+meta def insert_vertex (d : dag T) (a : T) : dag T :=
+if ¬ native.rb_map.contains d a then
+  native.rb_map.insert d a []
+else
+  d
+meta def insert_vertices (d : dag T) (a : list T) : dag T := a.foldl insert_vertex d
+variable [decidable_eq T]
+meta def insert_edge (d : dag T) (v w : T) : dag T := if w ∈ d.find v then d else insert_vertex (native.rb_lmap.insert d v w) w
+meta def insert_edges (d : dag T) (a : list (T × T)) : dag T := a.foldl (λ da ⟨v, w⟩, insert_edge da v w) d
+meta def vertices : dag T → list T := native.rb_map.keys
+meta def edges (d : dag T) : list (T × T) := native.rb_map.fold d []
+  (λ v es old, old ++ es.map (λ x, (v, x)))
+meta def erase_all (d : dag T) (l : list T) : dag T := l.foldl (λ o v, native.rb_map.fold (native.rb_map.erase o v) (native.rb_map.erase o v) (λ w ll oll, native.rb_map.insert oll w (ll.erase v))) d
+meta def num_edges (d : dag T) : ℕ := d.fold 0 (λ _ l o, o + l.length)
+
+variables [has_to_format T]
+open format prod native.rb_map
+private meta def format_key_data (k : T) (d : list T) (first : bool) : format :=
+(if first then to_fmt "" else to_fmt "," ++ line) ++ to_fmt "id :" ++ to_fmt k ++ space ++ to_fmt ":" ++ space ++ to_fmt d -- todo what symbol?
+
+meta instance : has_to_format (dag T) :=
+⟨λ m, group $ to_fmt "[" ++ nest 1 (fst (fold m (to_fmt "", tt) (λ k d p, (fst p ++ format_key_data k d (snd p), ff)))) ++
+              to_fmt "]"⟩
+meta instance : has_repr (dag T) := ⟨λ s, (has_to_format.to_format s).to_string⟩
+
+
+open native
+/-- Depth first search used for topological sorting. -/
+meta def dfs (d : dag T) : T → (list T × rb_set T) → (list T × rb_set T)
+-- vertex and stack, visited pair
+| v stavis :=
+  (λ a : list T × rb_set T, (v :: a.fst, a.snd))
+    ((d.find v).foldl
+      (λ stavis' w,
+        if stavis'.snd.contains w then
+          stavis'
+        else
+          dfs w stavis')
+      (stavis.fst, stavis.snd.insert v))
+-- #eval (((((dag.mk ℕ).insert_vertex 3).insert_edge 2 1).insert_edge 1 3).dfs 1 [] (rb_map.mk _ _)).fst
+-- TODO is this inefficient?
+/-- Take the sub-graph of things reachable from `v` -/
+meta def reachable (d : dag T) (v : T) : dag T :=
+(d.dfs v ([], mk_rb_map)).fst.foldl (λ ol w, rb_map.insert ol w $ d.find w) (dag.mk T)
+/-- Depth first search used for reachability lookup table. -/
+meta def dfs_reach_table (d : dag T) : T → rb_map T (rb_set T) → rb_map T (rb_set T)
+| v rea :=
+  let a := ((d.find v).foldl
+      (λ (rea' : rb_map T (rb_set T)) w, let n :=
+        if rea'.contains w then
+          rea'
+        else
+          dfs_reach_table w rea' in
+        n.insert v $ (((n.find v).get_or_else mk_rb_set).union $ (n.find w).get_or_else mk_rb_set))
+      rea) in a.insert v $ ((a.find v).get_or_else mk_rb_set).insert v
+
+meta def reachable_table (d : dag T) : rb_map T (rb_set T) :=
+d.fold mk_rb_map (λ v _ n, d.dfs_reach_table v n)
+-- #eval (((((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5), (5,6),(5,8),(8,7),(8,6), (6,7)]).reachable_table).find 5).get_or_else mk_rb_set).to_list
+-- | [] c := c
+-- | (n :: S) c := (d.find n).foldl
+--   (λ old ins,
+--     (native.rb_map.fold (reachable ins) old
+--       (λ k d a, a.insert_edges (d.map (λ v, (k, v))))).insert_edge n ins)
+--   ((dag.mk T).insert_vertex n)
+/- Take the sub-graph of things reachable from a list of names-/
+-- meta def reachable'_core (orig : dag T) : list T → native.rb_map T bool → dag T → dag T
+-- | (h :: l) vis d := (orig.find h).foldl (λ o w, reachable'_core l vis d) (vis.insert h tt)
+-- | [] _ d := d
+-- meta def reachable' : list T → dag T → dag T :=
+-- λ l d, reachable'_core l
+  -- (d.fold (native.rb_map.mk _ _) (λ v _ o, o.insert v false)) d
+-- λ n t,
+--   (t.find n).foldl
+--   (λ old ins,
+--     (native.rb_map.fold (reachable ins t) old
+--       (λ k d a, native.rb_map.insert a k d)).insert n ins)
+--   (dag.mk T)
+open native
+
+-- TODO change to rb_set?
+meta def minimal_vertices_dfs (d : dag T) : T → rb_map T bool → rb_map T bool → (rb_map T bool × rb_map T bool)
+| v minimals visited :=
+  -- (λ a : native.rb_map T bool × native.rb_map T bool, (a.fst.insert, a.snd))
+    ((d.find v).foldl
+      (λ ⟨mins, vis⟩ w,
+        if (vis.find w).get_or_else ff then
+          (mins.insert w ff, vis)
+        else
+          minimal_vertices_dfs w (mins.insert w ff) vis)
+      (minimals, visited.insert v tt))
+--#eval to_string (((((dag.mk ℕ).insert_vertex 3).insert_edge 2 1).insert_edge 2 3).minimal_vertices_dfs 1 (native.rb_map.mk _ _) (native.rb_map.mk _ _)).fst
+
+/-- Return the list of minimal vertices in a dag -/
+meta def minimal_vertices (d : dag T) (start : native.rb_set T) : rb_set T :=
+  rb_map.fold
+  (start.fold
+    ((-- minimal vertices
+      d.vertices.foldl (λ ol t, rb_map.insert ol t tt) (rb_map.mk T bool),
+      -- visited
+      d.vertices.foldl (λ ol t, rb_map.insert ol t ff) (rb_map.mk T bool)) :
+      rb_map T bool × rb_map T bool)
+    (λ (v : T) (minsvis : rb_map T bool × rb_map T bool),
+      if (minsvis.2.find v).get_or_else ff then
+        minsvis
+      else
+        minimal_vertices_dfs d v minsvis.1 minsvis.2)
+  ).fst (rb_map.mk _ _) (λ w b ol, if b && start.contains w then ol.insert w else ol)
+
+meta def merge_el (S : list (list T)) : option (list T) → option (list T) → list (list T)
+| none _ := S
+| _ none := S
+| (some u) (some v) := ((S.erase u).erase v).insert (u.union v)
+meta def merge (S : list (list T)) (u v : T) : list (list T) :=
+merge_el S (S.find (λ s : list T, u ∈ s))
+           (S.find (λ s : list T, v ∈ s))
+
+variable [inhabited T]
+meta def minimal_vertices_components_dfs (d : dag T) (t : T) : T → rb_map T bool → rb_map T T
+  → list (list T) → (rb_map T bool × rb_map T T × list (list T))
+| v minimals visited components :=
+    ((d.find v).foldl
+      (λ ⟨mins, vis, fcomps⟩ w,
+        let fcomps' := merge fcomps t w in
+        if h : vis.contains w then
+          -- TODO could replace with find_def w
+          (mins.insert w ff, vis, merge fcomps' t (vis.ifind w))
+        else
+          minimal_vertices_components_dfs w (mins.insert w ff) vis fcomps')
+      (minimals, visited.insert v t, components))
+--#eval _root_.to_string (((((dag.mk ℕ).insert_vertex 3).insert_edge 2 1).insert_edge 2 3).minimal_vertices_components_dfs
+  -- 2 2 ([1,2,3].foldl (λ o oo, o.insert oo tt) (rb_map.mk _ _))
+  --     ([3].foldl (λ o oo, o.insert oo 3) (rb_map.mk _ _))
+  --     ([1,2,3].foldl (λ o oo, o.insert ([oo])) ([]))
+  --     )
+
+
+
+/-- Return the list of minimal vertices in a dag -/
+meta def minimal_vertices_with_components_aux (d : dag T) (start : native.rb_set T) :
+  native.rb_map T bool × native.rb_map T T × list (list T) :=
+start.fold
+  ((-- minimal vertices
+    d.vertices.foldl (λ ol t, native.rb_map.insert ol t tt) (native.rb_map.mk T bool),
+    -- visited
+    rb_map.mk T T,
+    -- components
+    start.fold [] (λ t ol, [t] :: ol)) :
+    native.rb_map T bool × native.rb_map T T × list (list T))
+  (λ (v : T) (minsviscomp : native.rb_map T bool × native.rb_map T T × list (list T)),
+  -- _root_.trace_val $
+    if minsviscomp.2.1.contains v then -- already visited
+      minsviscomp
+    else
+      minimal_vertices_components_dfs d v v minsviscomp.1 minsviscomp.2.1 minsviscomp.2.2)
+
+meta def minimal_vertices_with_components (d : dag T) (start : native.rb_set T) :
+  rb_set T × list (list T) :=
+(λ ans : native.rb_map T bool × native.rb_map T T × list (list T),
+  ((ans.fst.fold mk_rb_set (λ w b ol, if b && start.contains w then ol.insert w else ol),
+    ans.snd.snd) : rb_set T × list (list T)))
+(minimal_vertices_with_components_aux d start)
+--#eval to_string ((((dag.mk ℕ).insert_vertex 3).insert_edges [(1, 5), (3, 2), (4,5), (2,5)]).minimal_vertices_with_components_aux (rb_set.of_list [1,3,4])).2.2
+
+/-- Return a topological sort of the DAG. -/
+meta def topological_sort (d : dag T) : list T :=
+  (native.rb_map.fold d
+    (([] : list T), mk_rb_set)
+    (λ v es stavis,
+      if stavis.snd.contains v then
+        stavis
+      else
+        d.dfs v stavis)
+  ).fst
+-- meta def topological_sort' (d : dag T) [has_to_string T]:tactic unit :=
+-- do
+--   native.rb_map.mfold d
+--     (([] : list T), d.vertices.foldl (λ ol t, native.rb_map.insert ol t ff) (native.rb_map.mk T bool))
+--     (λ v es ⟨stack, vis⟩,
+--       if (vis.find v).get_or_else ff then
+--         do
+--         tactic.trace "a",
+--         tactic.trace $ to_string v,
+--         tactic.trace $ to_string stack,
+--         tactic.trace $ to_string vis,
+--         return ⟨stack, vis⟩
+--       else
+--         do
+--         tactic.trace "b",
+--         tactic.trace $ to_string v,
+--         return $ dfs d v stack vis),
+--     return ()
+-- #eval (λ d : dag _, (d.dfs 5 ([] : list _) (d.vertices.foldl (λ ol t, native.rb_map.insert ol t ff) (native.rb_map.mk _ bool))).fst) (((dag.mk ℕ).insert_vertices [0, 1, 2, 3, 4, 5]).insert_edges [(5, 2), (5,0),(4,0),(4,1),(2,3),(3,1)])
+--#eval (((dag.mk ℕ).insert_vertices [0, 1, 2]).insert_edges [(0, 1), (2,1),(0,2)]).topological_sort
+-- run_cmd (((dag.mk ℕ).insert_vertices [0, 1, 2]).insert_edges [(0, 1), (2,1),(0,2)]).topological_sort'
+
+-- [id :linear_ordered_add_comm_group : [linear_ordered_cancel_add_comm_monoid, linear_order],
+--  id :linear_order : [],
+--  id :linear_ordered_cancel_add_comm_monoid : [linear_order]]
+/-
+Return the prefix of l up to and including the first element satisfying P
+-/
+def slice_up_to {α : Type*} (P : α → Prop) [decidable_pred P] : list α → list α
+| [] := []
+| (a :: l) := if P a then [a] else a :: slice_up_to l
+--#eval slice_up_to (∈ [1,2,4]) [5,5,612,4,12,1]
+
+/-
+Is vertex w reachable from v in the dag d, given ts a topological sort of d.
+-/
+meta def is_reachable [has_to_string T] (d : dag T) (ts : list T) (v w : T) : bool :=
+-- _root_.trace (_root_.to_string v) $
+-- _root_.trace (_root_.to_string w) $
+(d.reachable v).contains w
+/-- are all vertices in `S` inside dag `d` with reachable table `dr` reachable from `v` -/
+meta def all_reachable [has_to_string T] (d : dag T) (dr : rb_map T (rb_set T)) (v : T) (S : list T) : bool :=
+-- _root_.trace (_root_.to_string v) $
+-- _root_.trace (_root_.to_string S) $
+-- _root_.trace (has_to_format.to_format dreach).to_string $
+S.foldl (λ (ol : bool) (w : T), ol && ((dr.find v).get_or_else mk_rb_set).contains w) tt
+-- TODO this is hilariously inefficient
+--#eval ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5)]).is_reachable [] 3 3
+/-- are all vertices in `S` inside dag `d` with topological sort `ts` reachable from `v` -/
+meta def all_reachable' [has_to_string T] (d : dag T) (ts : list T) (v : T) (S : list T) : bool :=
+-- _root_.trace (_root_.to_string v) $
+-- _root_.trace (_root_.to_string S) $
+let dreach := d.reachable v in
+-- _root_.trace (has_to_format.to_format dreach).to_string $
+S.foldl (λ (ol : bool) (w : T), ol && dreach.contains w) tt
+-- TODO this is hilariously inefficient
+--#eval ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5)]).is_reachable [] 3 3
+
+meta def meet [has_to_string T] (d : dag T) (ts : list T) (dr : rb_map T (rb_set T)) (S : list T) : T :=
+-- trace (to_string ts)
+((slice_up_to (∈ S) ts).reverse.find (λ v, d.all_reachable dr v S)).iget
+
+meta def meet' [has_to_string T] (d : dag T) (S : list T) : T := let ts := d.topological_sort in
+-- trace (to_string ts)
+((slice_up_to (∈ S) ts).reverse.find (λ v, d.all_reachable' ts v S)).iget
+
+--#eval ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5),(7,1),(7,3)]).meet [3,1]
+meta def meets_of_components [has_to_string T] (d : dag T) (ts : list T) (dr : rb_map T (rb_set T)) (start : native.rb_set T) :
+  native.rb_set T :=
+rb_set.of_list $ (d.minimal_vertices_with_components start).snd.map (λ S, d.meet ts dr S)
+meta def meets_of_components' [has_to_string T] (d : dag T) (start : native.rb_set T) :
+  native.rb_set T :=
+rb_set.of_list $ (d.minimal_vertices_with_components start).snd.map (λ S, d.meet' S)
+--#eval rb_set.to_list $ ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5),(7,1),(7,3),(7,6), (6,8)]).meets_of_components
+  -- (rb_set.of_list [4,7])
+--#eval ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5),(7,1),(7,3), (6,8)]).meet [8]
+--#eval ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5),(7,1),(7,3), (6,8)]).reachable 8
+
+end dag
--- src/file_splitter.lean
+++ src/file_splitter.lean
@@ -3,8 +3,39 @@ import data.vector
 import crawler
 import tactic
 import number_theory.quadratic_reciprocity
+import data.list.lex
 
 open tactic declaration environment io io.fs (put_str_ln close)
+
+
+/-- pre should have a slash at the end -/
+def import_to_file (pre : string) (im : name) : string :=
+pre ++ im.to_string_with_sep "/" ++ ".lean"-- TODO windows lol
+
+/-- pre should have a slash at the end -/
+def file_to_import (pre : string) (file : string) : name :=
+name.from_components
+((((string.get_rest file pre).get_or_else "Hello, I'm trapped in an error string, please let me out").split_on '.').head.split_on '/')
+
+/-- A hackish way to get the `src` directory of any project.
+  Requires as argument any declaration name `n` in that project, and `k`, the number of characters
+  in the path of the file where `n` is declared not part of the `src` directory.
+  Example: For `mathlib_dir_locator` this is the length of `tactic/project_dir.lean`, so `23`.
+  Note: does not work in the file where `n` is declared. -/
+meta def environment.get_project_dir (e : environment) (n : name) (k : ℕ) : string :=
+(do
+  s ← e.decl_olean n,
+  return $ s.popn_back k).get_or_else "Hello, I'm trapped in an error string, please let me out"
+
+/-- A hackish way to get the `src` directory of mathlib. -/
+meta def environment.get_mathlib_dir (e : environment) : string :=
+e.get_project_dir `mathlib_dir_locator 23
+
+/-- Checks whether a declaration with the given name is declared in mathlib.
+If you want to run this tactic many times, you should use `environment.is_prefix_of_file` instead,
+since it is expensive to execute `get_mathlib_dir` many times. -/
+meta def environment.is_in_mathlib (e : environment) (n : name) : bool :=
+e.is_prefix_of_file e.get_mathlib_dir n
 
 
 structure import_data : Type :=
@@ -53,10 +84,12 @@ let name := decl.to_name,
     file_pos := pos,
     deps := deps, }
 
-meta def mk_file_data (env : environment) (fname : string) : list import_data :=
-(env.get_decls.filter (λ d : declaration, env.decl_olean d.to_name = fname)).map (mk_data env)
+meta def mk_file_data (env : environment) (fname : name) : list import_data :=
+(env.get_decls.filter
+  (λ d : declaration, env.decl_olean d.to_name = import_to_file env.get_mathlib_dir fname)).map
+    (mk_data env)
 
-meta def mk_file_dag (env : environment) (fname : string) : dag import_data :=
+meta def mk_file_dag (env : environment) (fname : name) : dag import_data :=
 let fdata := mk_file_data env fname,
     decl_names := fdata.map import_data.decl_name in
 fdata.foldl (λ G id,
@@ -79,53 +112,22 @@ rb_map.mk _ _
 meta instance [has_to_format T] : has_to_format (rb_counter T) := rb_map.has_to_format
 end rb_counter
 end rb_counter
-
-/-- A hackish way to get the `src` directory of any project.
-  Requires as argument any declaration name `n` in that project, and `k`, the number of characters
-  in the path of the file where `n` is declared not part of the `src` directory.
-  Example: For `mathlib_dir_locator` this is the length of `tactic/project_dir.lean`, so `23`.
-  Note: does not work in the file where `n` is declared. -/
-meta def environment.get_project_dir (e : environment) (n : name) (k : ℕ) : option string :=
-do
-  s ← e.decl_olean n,
-  return $ s.popn_back k
-
-/-- A hackish way to get the `src` directory of mathlib. -/
-meta def environment.get_mathlib_dir (e : environment) : string :=
-(e.get_project_dir `mathlib_dir_locator 23).get_or_else "Hello, I'm trapped in an error string, please let me out"
-
-/-- Checks whether a declaration with the given name is declared in mathlib.
-If you want to run this tactic many times, you should use `environment.is_prefix_of_file` instead,
-since it is expensive to execute `get_mathlib_dir` many times. -/
-meta def environment.is_in_mathlib (e : environment) (n : name) : bool :=
-e.is_prefix_of_file e.get_mathlib_dir n
-
 open tactic
-meta def mk_file_dep_counts (env : environment) (fname : string) : rb_counter string :=
+
+meta def mk_file_dep_counts_basic (env : environment) (fname : name) : rb_counter name :=
 let G := mk_file_dag env fname,
     Gr := G.reachable_table in
 Gr.fold (rb_counter.mk _) (λ k d o, k.deps.foldl
   (λ o2 dep, if env.is_in_mathlib dep then o2.incr_by (file_name $ env.decl_olean dep) d.size else o2) o)
 
 
-run_cmd (do
-  e ← get_env,
-  -- d← get_decl `vector.to_list_append,
-  let a := mk_file_dag e (file_name (e.decl_olean `list.split_at)),
-  trace a,
-  -- trace a.reachable_table,
-  trace $ mk_file_dep_counts e (file_name (e.decl_olean `zmod.quadratic_reciprocity)),
-  -- trace $ mk_data e d,
-  skip
-)
+meta def io.handle.get_line_as_string (f : handle) : io string :=
+do g ← fs.get_line f, return g.to_list.as_string
 
-meta def io.handle.get_line_as_string (f : handle) : tactic string :=
-do g ← unsafe_run_io $ fs.get_line f, return g.to_list.as_string
-
-meta def get_imports_aux : handle → bool → tactic (list name)
+meta def get_imports_aux : handle → bool → io (list name)
 | f b :=
 do
-  eo ← unsafe_run_io $ io.fs.is_eof f,
+  eo ← io.fs.is_eof f,
   if eo then return []
   else do
     l ← f.get_line_as_string,
@@ -139,51 +141,105 @@ do
       else
         get_imports_aux f b
 
-/-- pre should have a slash at the end -/
-def import_to_file (pre : string) (im : name) : string :=
-pre ++ im.to_string_with_sep "/" ++ ".lean"-- TODO windows lol
-
-/-- pre should have a slash at the end -/
-def file_to_import (pre : string) (file : string) : name :=
-".".intercalate
-((((string.get_rest file pre).get_or_else "Hello, I'm trapped in an error string, please let me out").split_on '.').head.split_on '/')
-
-meta def get_imports (file : name) : tactic (list name) :=
-do e ← get_env,
-  f ← unsafe_run_io $ mk_file_handle (import_to_file e.get_mathlib_dir file) io.mode.read,
+meta def get_imports (e : environment) (file : name) : io (list name) :=
+do
+  f ← mk_file_handle (import_to_file e.get_mathlib_dir file) io.mode.read <|>
+                      mk_file_handle (import_to_file e.get_mathlib_dir $ file.append `default) io.mode.read <|>
+                      mk_file_handle (import_to_file (e.get_project_dir `nat 14) file) io.mode.read <|>
+                      mk_file_handle (import_to_file (e.get_project_dir `nat 14) $ file.append `default) io.mode.read,
   get_imports_aux f ff
 
-meta def get_dag_aux : name → dag name → tactic (dag name)
+meta def get_dag_aux (e : environment) : name → dag name → io (dag name)
 | n d := do
-  l ← get_imports n,
-  l.mfoldl (λ od im, do G ← (get_dag_aux im od), return $ G.insert_edge n im) d
+  l ← get_imports e n,
+  l.mfoldl (λ od im, do
+    G ← if od.contains im then return od else get_dag_aux im od,
+    return $ G.insert_edge n im) d
 
-meta def get_dag (file : name) : tactic (dag name) :=
-do e ← get_env,
-  get_dag_aux file (dag.mk _)
+meta def get_import_dag (e : environment) (file : name) : io (dag name) :=
+get_dag_aux e file (dag.mk _)
+
+meta def mk_file_dep_counts (env : environment) (fname : name) : io (rb_counter name) :=
+do let dc := mk_file_dep_counts_basic env fname,
+  G ← get_import_dag env fname,
+  let Gr := G.reachable_table,
+  return $ Gr.fold dc (λ na ln odc, ln.fold odc (λ de odc', odc'.incr_by de ((dc.find na).get_or_else 0)))
+
+open native
+
+-- meta def dfs_all_paths' {T : Type*} [has_lt T] [decidable_rel ((<) : T → T → Prop)] [decidable_eq T] (d : dag T)
+--   : T → (list (list T) × rb_set T) → (list (list T) × rb_set T)
+-- -- vertex and stack, visited pair
+-- | v stavis :=
+--   (λ a : list (list T) × rb_set T, (a.fst.map ((::) v), a.snd))
+--     ((d.find v).foldl
+--       (λ stavis' w,
+--         if stavis'.snd.contains w then
+--           stavis'
+--         else
+--           dfs_all_paths' w stavis')
+--       (stavis.fst, stavis.snd.insert v))
+/-- Depth first search all paths. -/
+meta def dfs_all_paths {T : Type*} [has_lt T] [decidable_rel ((<) : T → T → Prop)] [decidable_eq T]
+  (d : dag T) (tgt : T) : T → rb_lmap T (list T) → rb_lmap T (list T)
+| v paths :=
+    (d.find v).foldl (λ opa de,
+    let npa :=
+      if paths.contains de then opa else dfs_all_paths de opa in
+        rb_map.insert npa v $ (npa.find de).map ((::) v)) paths
+      -- else
+      --   (dfs_all_paths de opa).map (λ p, v :: p)) paths
+  -- let a := ((d.find v).foldl
+      -- (λ (rea' : rb_map T (list T)) w, let n :=
+      --   if rea'.contains w then
+      --     rea'
+      -- ()  else
+      --     dfs_reach_table w rea' in
+      --   n.insert v $ (((n.find v).get_or_else mk_rb_set).union $ (n.find w).get_or_else mk_rb_set))
+      -- rea) in a.insert v $ ((a.find v).get_or_else mk_rb_set).insert v
+
+meta def all_paths {T : Type*} [has_lt T] [decidable_rel ((<) : T → T → Prop)] [decidable_eq T]
+(d : dag T) (src tgt : T) :
+  list (list T) :=
+(dfs_all_paths d tgt src $ (mk_rb_map).insert tgt [[tgt]]).find src
+#eval all_paths ((dag.mk ℕ).insert_edges [(1, 5), (3, 2), (4,5), (2,5), (5,6),(5,8),(8,7),(8,6), (5,19),(19,7), (6,7)]) 1 7
 
 
 run_cmd (do
   e ← get_env,
-  let nam:=`data.list.defs,
+  -- d← get_decl `vector.to_list_append,
+  a ←  unsafe_run_io $ get_import_dag e $ file_to_import e.get_mathlib_dir (file_name (e.decl_olean `zmod.quadratic_reciprocity)),
+  -- trace a,
+  trace (all_paths a `algebra.algebra.basic `tactic.abel),
+  trace (all_paths a `number_theory.quadratic_reciprocity `category_theory.whiskering),
+  -- trace a.reachable_table ,
+  -- trace $ import_to_file e.get_mathlib_dir $ file_to_import e.get_mathlib_dir$file_name (e.decl_olean `zmod.quadratic_reciprocity),
+  -- trace $ unsafe_run_io $ mk_file_dep_counts e $ file_to_import e.get_mathlib_dir (file_name (e.decl_olean `zmod.quadratic_reciprocity)),
+  -- trace $ mk_data e d,
+  skip
+)
+
+run_cmd (do
+  e ← get_env,
+  let nam:=`tactic.basic,
   -- f ← unsafe_run_io $ mk_file_handle (import_to_file e.get_mathlib_dir nam) io.mode.read,
-  trace $ get_dag nam
+  trace $ unsafe_run_io $ get_import_dag e nam
   -- trace a
   -- let b := a.head,
   -- trace ((import_to_file e.get_mathlib_dir b)),
   -- trace (file_to_import e.get_mathlib_dir (import_to_file e.get_mathlib_dir b))
 )
 
-meta def main : tactic unit :=
-do curr_env ← get_env,
-   h ← unsafe_run_io (mk_file_handle "data.yaml" mode.write),
-   let decls := curr_env.fold [] list.cons,
-   let filtered_decls := decls.filter
-     (λ x, not (to_name x).is_internal),
-   filtered_decls.mmap' (λ d,
-     do s ← (print_item_crawl curr_env d),
-        unsafe_run_io (do io.fs.put_str_ln h s,
-                          io.fs.flush h),
-        skip),
-   unsafe_run_io (close h),
-   skip
+-- meta def main : tactic unit :=
+-- do curr_env ← get_env,
+--    h ← unsafe_run_io (mk_file_handle "data.yaml" mode.write),
+--    let decls := curr_env.fold [] list.cons,
+--    let filtered_decls := decls.filter
+--      (λ x, not (to_name x).is_internal),
+--    filtered_decls.mmap' (λ d,
+--      do s ← (print_item_crawl curr_env d),
+--         unsafe_run_io (do io.fs.put_str_ln h s,
+--                           io.fs.flush h),
+--         skip),
+--    unsafe_run_io (close h),
+--    skip
